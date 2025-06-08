0e8c5417e48489ccafeb6495b570d60e1f84d222
MyWish <myEmail@example.com>
2025-06-08 23:03:30 +0530

    Initial commit

--- src/ex_01_Java_Basics/Lab002_Hw.java
+++ src/ex_01_Java_Basics/Lab002_Hw.java
@@ -1,7 +1,7 @@
 package ex_01_Java_Basics;
 
-public class Lab002_HW {
+public class Lab002_Hw {
     public static void main(String[] args) {
-
+        System.out.println("hello world 2");
     }
 }
