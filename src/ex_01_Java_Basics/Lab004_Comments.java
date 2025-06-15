7bdccab4499ac54279d2f075d6b01762020f134c
MyWish <myEmail@example.com>
2025-06-15 23:59:16 +0530

    Initial commit

---
+++ src/ex_01_Java_Basics/Lab004_Comments.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basics;
+
+public class Lab004_Comments {
+    // This is a comment, this code will nto be executed.
+         /*System.out.println("Hi");
+         System.out.println("Bye");
+         System.out.println("Drink");
+         System.out.println("Chai");*/
+}
